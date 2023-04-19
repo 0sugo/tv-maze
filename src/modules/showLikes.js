e9ddc3c01e5ac63ab4ef52bf42155df63de56a72
rosemutai <chepngetichrose2030@gmail.com>
2023-04-19 23:07:00 +0300

    Display the likes of each show

---
+++ src/modules/showLikes.js
@@ -0,0 +1,24 @@
+const url = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/LR60RRSADfy5uTrj8R5e/likes';
+
+const fetchLikes = async (name, h5Tag) => {
+  try {
+    const response = await fetch(url);
+    const result = await response.json();
+    result.forEach((el) => {
+      if (el.item_id === name) {
+        const countOfLikes = el.likes;
+        if (countOfLikes === 1) {
+          h5Tag.innerHTML = `${countOfLikes} like`;
+        } else if (countOfLikes === 1) {
+          h5Tag.innerHTML = '0 likes';
+        } else {
+          h5Tag.innerHTML = `${countOfLikes} likes`;
+        }
+      }
+    });
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+export default fetchLikes;
