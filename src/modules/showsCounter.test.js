3b6aba180f16be335ad5173eea3d93d70f9538f5
rosemutai <chepngetichrose2030@gmail.com>
2023-04-21 22:27:21 +0300

    Test shows counter

---
+++ src/modules/showsCounter.test.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { TextEncoder, TextDecoder } from 'text-encoding';
+
+import getShowsCount from './showsCounter.js';
+
+Object.assign(global, { TextEncoder, TextDecoder });
+
+describe('get length of shows', () => {
+  let dom;
+
+  beforeEach(() => {
+    // eslint-disable-next-line import/no-extraneous-dependencies, global-require
+    document.body.innerHTML = `
+        <a href="" class="menu-link" >
+          Movies<span id="shows-count"></span>
+        </a>
+        <div id="shows-count">
+          <div class="show-card"></div>
+          <div class="show-card"></div>
+          <div class="show-card"></div>
+        </div>
+    `;
+  });
+
+  it('should get the length of shows', () => {
+    const countTag = document.getElementById('shows-count');
+    const shows = document.getElementsByClassName('show-card');
+    getShowsCount();
+    countTag.innerHTML = shows.length;
+    expect(countTag.innerHTML).toContain('3');
+  });
+});
